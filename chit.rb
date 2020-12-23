de1dc68d6588d32f8014ab030e3ecf9c4f06e122
Tom Craig <thetomcraig@icloud.com>
2020-12-22 21:20:25 -0800

    v9

--- chit.rb
+++ chit.rb
@@ -1,8 +1,8 @@
 class Chit < Formula
   desc "Chane your terminal color schemes"
   homepage "https://github.com/thetomcraig/Chit"
-  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.0.8.tar.gz?raw=true"
-  sha256 "5633d5ade3afdaa6f2d788e4349d0b26ecfafd3daec5c6312386b0155785bc4c"
+  url "https://github.com/thetomcraig/chit/blob/main/archive/chit.0.0.9.tar.gz?raw=true"
+  sha256 "64034bf019de4a65bd767c168bce3fb2532e2146d21d2a59bbadc7dbbba694fa"
   license "BSD-3-Clause"
 
   bottle :unneeded
@@ -11,7 +11,7 @@ class Chit < Formula
     system "./build"
     bin.install "chit"
     prefix.install Dir["example_theme_definitions"]
-    system "./copy_themes"
+    sy stem "./copy_themes"
   end
 
 end
